67046a682f847e847082336cdc08fa1dd272d149
Marco Ramirez <mvrcentes@hotmail.com>
2022-10-16 13:35:53 -0600

    SQL Basics

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/sqlbasics/CaliforniaPark.java
@@ -0,0 +1,124 @@
+package com.example.sqlbasics;
+
+import java.lang.System;
+
+@androidx.room.Entity(tableName = "park")
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000(\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0004\n\u0002\u0010\t\n\u0002\b\u0018\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0087\b\u0018\u00002\u00020\u0001B?\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u0012\u0006\u0010\u0006\u001a\u00020\u0005\u0012\u0006\u0010\u0007\u001a\u00020\u0003\u0012\b\u0010\b\u001a\u0004\u0018\u00010\u0003\u0012\u0006\u0010\t\u001a\u00020\n\u0012\u0006\u0010\u000b\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\fJ\t\u0010\u0019\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001a\u001a\u00020\u0005H\u00c6\u0003J\t\u0010\u001b\u001a\u00020\u0005H\u00c6\u0003J\t\u0010\u001c\u001a\u00020\u0003H\u00c6\u0003J\u0010\u0010\u001d\u001a\u0004\u0018\u00010\u0003H\u00c6\u0003\u00a2\u0006\u0002\u0010\u0017J\t\u0010\u001e\u001a\u00020\nH\u00c6\u0003J\t\u0010\u001f\u001a\u00020\u0005H\u00c6\u0003JV\u0010 \u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u00052\b\b\u0002\u0010\u0007\u001a\u00020\u00032\n\b\u0002\u0010\b\u001a\u0004\u0018\u00010\u00032\b\b\u0002\u0010\t\u001a\u00020\n2\b\b\u0002\u0010\u000b\u001a\u00020\u0005H\u00c6\u0001\u00a2\u0006\u0002\u0010!J\u0013\u0010\"\u001a\u00020#2\b\u0010$\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010%\u001a\u00020\u0003H\u00d6\u0001J\t\u0010&\u001a\u00020\u0005H\u00d6\u0001R\u0016\u0010\u0007\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\r\u0010\u000eR\u0016\u0010\u0006\u001a\u00020\u00058\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000f\u0010\u0010R\u0016\u0010\t\u001a\u00020\n8\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0011\u0010\u0012R\u0016\u0010\u0002\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0013\u0010\u000eR\u0016\u0010\u0004\u001a\u00020\u00058\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0010R\u0016\u0010\u000b\u001a\u00020\u00058\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0015\u0010\u0010R\u001a\u0010\b\u001a\u0004\u0018\u00010\u00038\u0006X\u0087\u0004\u00a2\u0006\n\n\u0002\u0010\u0018\u001a\u0004\b\u0016\u0010\u0017\u00a8\u0006\'"}, d2 = {"Lcom/example/sqlbasics/CaliforniaPark;", "", "id", "", "name", "", "city", "acres", "visitors", "established", "", "type", "(ILjava/lang/String;Ljava/lang/String;ILjava/lang/Integer;JLjava/lang/String;)V", "getAcres", "()I", "getCity", "()Ljava/lang/String;", "getEstablished", "()J", "getId", "getName", "getType", "getVisitors", "()Ljava/lang/Integer;", "Ljava/lang/Integer;", "component1", "component2", "component3", "component4", "component5", "component6", "component7", "copy", "(ILjava/lang/String;Ljava/lang/String;ILjava/lang/Integer;JLjava/lang/String;)Lcom/example/sqlbasics/CaliforniaPark;", "equals", "", "other", "hashCode", "toString", "app_debug"})
+public final class CaliforniaPark {
+    @androidx.room.PrimaryKey(autoGenerate = true)
+    private final int id = 0;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo(name = "name")
+    private final java.lang.String name = null;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo(name = "city")
+    private final java.lang.String city = null;
+    @androidx.room.ColumnInfo(name = "area_acres")
+    private final int acres = 0;
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.ColumnInfo(name = "park_visitors")
+    private final java.lang.Integer visitors = null;
+    @androidx.room.ColumnInfo(name = "established")
+    private final long established = 0L;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo(name = "type")
+    private final java.lang.String type = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.sqlbasics.CaliforniaPark copy(int id, @org.jetbrains.annotations.NotNull()
+    java.lang.String name, @org.jetbrains.annotations.NotNull()
+    java.lang.String city, int acres, @org.jetbrains.annotations.Nullable()
+    java.lang.Integer visitors, long established, @org.jetbrains.annotations.NotNull()
+    java.lang.String type) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object other) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    public CaliforniaPark(int id, @org.jetbrains.annotations.NotNull()
+    java.lang.String name, @org.jetbrains.annotations.NotNull()
+    java.lang.String city, int acres, @org.jetbrains.annotations.Nullable()
+    java.lang.Integer visitors, long established, @org.jetbrains.annotations.NotNull()
+    java.lang.String type) {
+        super();
+    }
+    
+    public final int component1() {
+        return 0;
+    }
+    
+    public final int getId() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getName() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getCity() {
+        return null;
+    }
+    
+    public final int component4() {
+        return 0;
+    }
+    
+    public final int getAcres() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.Integer component5() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.Integer getVisitors() {
+        return null;
+    }
+    
+    public final long component6() {
+        return 0L;
+    }
+    
+    public final long getEstablished() {
+        return 0L;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component7() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getType() {
+        return null;
+    }
+}
